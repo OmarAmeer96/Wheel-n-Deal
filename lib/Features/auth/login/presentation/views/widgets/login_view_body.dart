fc7bb790657293f5a8fd9d72af67b38d8f6ad647
Omar_Ameer <omar.ameer244@gmail.com>
2023-12-12 01:08:15 +0200

    Added SingleChildScrollView Widget.

--- lib/Features/auth/login/presentation/views/widgets/login_view_body.dart
+++ lib/Features/auth/login/presentation/views/widgets/login_view_body.dart
@@ -17,104 +17,106 @@ class LoginViewBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Welcome back! Glad to see you, Again!",
-              style: Styles.manropeRegular38,
-            ),
-            const SizedBox(
-              height: 66,
-            ),
-            CustomTextField(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 13),
-                child: SvgPicture.asset(
-                  AssetsData.userName,
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              controller: loginUserNameController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Name is required';
-                }
-                return null;
-              },
-              labelText: "Enter Name",
-              headerTextField: 'Username',
-              labelColor: const Color(0xffA4A4A4),
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            CustomTextField(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 13),
-                child: SvgPicture.asset(
-                  AssetsData.passWord,
-                ),
+              const Text(
+                "Welcome back! Glad to see you, Again!",
+                style: Styles.manropeRegular38,
               ),
-              controller: loginPasswordController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Password is required';
-                }
-                return null;
-              },
-              labelText: "Enter Password",
-              headerTextField: 'Password',
-              labelColor: const Color(0xffA4A4A4),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "Forgot Password",
-                    style: Styles.manropeRegular12.copyWith(
-                      color: const Color(0xff6A707C),
+              const SizedBox(
+                height: 66,
+              ),
+              CustomTextField(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 13),
+                  child: SvgPicture.asset(
+                    AssetsData.userName,
+                  ),
+                ),
+                controller: loginUserNameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Name is required';
+                  }
+                  return null;
+                },
+                labelText: "Enter Name",
+                headerTextField: 'Username',
+                labelColor: const Color(0xffA4A4A4),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              CustomTextField(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 13),
+                  child: SvgPicture.asset(
+                    AssetsData.passWord,
+                  ),
+                ),
+                controller: loginPasswordController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Password is required';
+                  }
+                  return null;
+                },
+                labelText: "Enter Password",
+                headerTextField: 'Password',
+                labelColor: const Color(0xffA4A4A4),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Forgot Password",
+                      style: Styles.manropeRegular12.copyWith(
+                        color: const Color(0xff6A707C),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            CustomMainButton(
-              text: "Login",
-              onPressed: () {},
-              color: kPrimaryColor,
-            ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don’t have an account?   ",
-                  style: Styles.manropeRegular12.copyWith(
-                    color: const Color(0xff191D31),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "Register Now",
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              CustomMainButton(
+                text: "Login",
+                onPressed: () {},
+                color: kPrimaryColor,
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don’t have an account?   ",
                     style: Styles.manropeRegular12.copyWith(
-                      color: kPrimaryColor,
+                      color: const Color(0xff191D31),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 26,
-            )
-          ],
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Register Now",
+                      style: Styles.manropeRegular12.copyWith(
+                        color: kPrimaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 26,
+              )
+            ],
+          ),
         ),
       ),
     );
