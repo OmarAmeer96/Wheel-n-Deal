92a311318c0b20fb03b32a23a214b77cbb2572a9
Omar_Ameer <omar.ameer244@gmail.com>
2023-12-11 20:34:49 +0200

    OnBoarding Screens Done Part1.

--- lib/Features/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/Features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -1,10 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return OnBoardingSlider(
+      finishButtonText: 'Register',
+      onFinish: () {},
+      finishButtonStyle: const FinishButtonStyle(
+        backgroundColor: Colors.deepOrange,
+      ),
+      skipTextButton: const Text(
+        'Skip',
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.deepOrange,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailing: const Text(
+        'Login',
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.deepOrange,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      trailingFunction: () {},
+      controllerColor: Colors.deepOrange,
+      totalPage: 3,
+      headerBackgroundColor: Colors.white,
+      pageBackgroundColor: Colors.white,
+      background: [
+        Image.asset(
+          'assets/images/test.png',
+          height: 400,
+        ),
+        Image.asset(
+          'assets/images/test.png',
+          height: 400,
+        ),
+        Image.asset(
+          'assets/images/test.png',
+          height: 400,
+        ),
+      ],
+      speed: 1.8,
+      pageBodies: [
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'On your way...',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'to find the perfect looking Onboarding for your app?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'You’ve reached your destination.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Sliding with animation',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Start now!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Where everything is possible and customize your onboarding.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
