54ebd5e486c5d97890d4bcd96860f521f043dac1
Omar_Ameer <omar.ameer244@gmail.com>
2023-12-17 15:41:40 +0200

    Edited the Navigation Bar.

--- lib/Features/user/home/views/user_home_view.dart
+++ lib/Features/user/home/views/user_home_view.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:wheel_n_deal/Core/utils/assets.dart';
+import 'package:wheel_n_deal/Core/utils/styles.dart';
 import 'package:wheel_n_deal/Features/user/home/views/widgets/user_home_view_body.dart';
 import 'package:wheel_n_deal/constants.dart';
 
@@ -23,30 +24,22 @@ class _UserHomeViewState extends State<UserHomeView> {
         key: _bottomNavigationKey,
         index: 0,
         height: 60.0,
-        items: <Widget>[
-          SvgPicture.asset(
-            AssetsData.homeIcon,
-            // ignore: deprecated_member_use
-            color: Colors.white,
-            width: 26,
+        items: const <Widget>[
+          NavBarItem(
+            text: "Home",
+            icon: AssetsData.homeIcon,
           ),
-          SvgPicture.asset(
-            AssetsData.searchIcon,
-            // ignore: deprecated_member_use
-            color: Colors.white,
-            width: 26,
+          NavBarItem(
+            text: "Search",
+            icon: AssetsData.searchIcon,
           ),
-          SvgPicture.asset(
-            AssetsData.othersIcon,
-            // ignore: deprecated_member_use
-            color: Colors.white,
-            width: 26,
+          NavBarItem(
+            text: "Others",
+            icon: AssetsData.othersIcon,
           ),
-          SvgPicture.asset(
-            AssetsData.profileIcon,
-            // ignore: deprecated_member_use
-            color: Colors.white,
-            width: 26,
+          NavBarItem(
+            text: "Profile",
+            icon: AssetsData.profileIcon,
           ),
         ],
         color: const Color(0xFF1D272F),
@@ -61,27 +54,62 @@ class _UserHomeViewState extends State<UserHomeView> {
         },
         letIndexChange: (index) => true,
       ),
-      body: Container(
-        color: Colors.blueAccent,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(page.toString(), textScaleFactor: 10.0),
-              ElevatedButton(
-                child: const Text('Go To Page of index 1'),
-                onPressed: () {
-                  final CurvedNavigationBarState? navBarState =
-                      _bottomNavigationKey.currentState;
-                  navBarState?.setPage(1);
-                },
-              )
-            ],
-          ),
-        ),
-      ),
+      body:
 
-      // const UserHomeViewBody(),
+          // Container(
+          //   color: Colors.blueAccent,
+          //   child: Center(
+          //     child: Column(
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: <Widget>[
+          //         Text(page.toString(), textScaleFactor: 10.0),
+          //         ElevatedButton(
+          //           child: const Text('Go To Page of index 1'),
+          //           onPressed: () {
+          //             final CurvedNavigationBarState? navBarState =
+          //                 _bottomNavigationKey.currentState;
+          //             navBarState?.setPage(1);
+          //           },
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          // ),
+
+          const UserHomeViewBody(),
+    );
+  }
+}
+
+class NavBarItem extends StatelessWidget {
+  const NavBarItem({
+    super.key,
+    required this.text,
+    required this.icon,
+  });
+
+  final String text;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          icon,
+          // ignore: deprecated_member_use
+          color: Colors.white,
+          width: 22,
+        ),
+        Text(
+          text,
+          style: Styles.poppinsSemiBold16.copyWith(
+            color: Colors.white,
+            fontSize: 12,
+          ),
+        )
+      ],
     );
   }
 }
