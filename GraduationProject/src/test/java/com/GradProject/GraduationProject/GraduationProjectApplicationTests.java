0805aa23297c1bf8ef59a120d4ed34c98a1055c2
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2023-12-20 22:33:09 +0200

    update

---
+++ GraduationProject/src/test/java/com/GradProject/GraduationProject/GraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GradProject.GraduationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
