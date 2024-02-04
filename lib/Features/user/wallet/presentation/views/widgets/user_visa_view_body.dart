708a9860a23fff9eda1fe0b9069d1a9e8ad59545
Omar_Ameer <omar.ameer244@gmail.com>
2024-02-04 23:58:51 +0200

    User Visa Screen done P1.

--- lib/Features/user/wallet/presentation/views/widgets/user_visa_view_body.dart
+++ lib/Features/user/wallet/presentation/views/widgets/user_visa_view_body.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:wheel_n_deal/Core/widgets/custom_main_button.dart';
+import 'package:wheel_n_deal/constants.dart';
 
-class UserVisaViewBody extends StatelessWidget {
+class UserVisaViewBody extends StatefulWidget {
   const UserVisaViewBody({super.key});
+
+  @override
+  State<UserVisaViewBody> createState() => _UserVisaViewBodyState();
+}
+
+class _UserVisaViewBodyState extends State<UserVisaViewBody> {
+  String? cardNumber;
+  DateTime? expiryDate;
+  int? cvv;
+  String? name;
+
+  final _cardNumberController = TextEditingController();
+  final _expiryDateController = TextEditingController();
+  final _cvvController = TextEditingController();
+  final _nameController = TextEditingController();
+
+  final _form = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +29,24 @@ class UserVisaViewBody extends StatelessWidget {
         onTap: () {
           Focus.of(context).unfocus();
         },
-        child: const Padding(
-          padding: EdgeInsets.only(left: 16, right: 16),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Form(
+            key: _form,
+            child: Column(
+              children: [
+                const Spacer(),
+                CustomMainButton(
+                  text: "Add Card",
+                  onPressed: () {},
+                  color: kPrimaryColor,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
