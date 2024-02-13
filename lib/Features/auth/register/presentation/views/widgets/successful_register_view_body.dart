f877b0bf2ad998341b725dd35946878836eca566
Omar_Ameer <omar.ameer244@gmail.com>
2024-02-13 19:41:36 +0200

    Added Coma.

--- lib/Features/auth/register/presentation/views/widgets/successful_register_view_body.dart
+++ lib/Features/auth/register/presentation/views/widgets/successful_register_view_body.dart
@@ -73,7 +73,7 @@ class SuccessfulRegisterViewBody extends StatelessWidget {
                   color: kPrimaryColor,
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
