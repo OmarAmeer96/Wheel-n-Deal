61c64727190672a51306c7b26dab989004704fa6
Omar_Ameer <omar.ameer244@gmail.com>
2024-02-14 22:28:38 +0200

    User Stepper Step 1 Done.

--- lib/Features/user/post_order/presentation/views/widgets/step_one_content.dart
+++ lib/Features/user/post_order/presentation/views/widgets/step_one_content.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wheel_n_deal/Core/utils/is_valid_phone_number.dart';
 import 'package:wheel_n_deal/Core/utils/styles.dart';
+import 'package:wheel_n_deal/Core/widgets/custom_main_text_form_field.dart';
 import 'package:wheel_n_deal/Features/user/post_order/presentation/views/widgets/make_order_select_location_item.dart';
+import 'package:wheel_n_deal/Features/user/post_order/presentation/views/widgets/step_one_item.dart';
+import 'package:wheel_n_deal/constants.dart';
 
 class StepOneContent extends StatefulWidget {
   const StepOneContent({
@@ -18,7 +22,9 @@ class _StepOneContentState extends State<StepOneContent> {
   String? senderPhoneNumber;
   String? receiverPhoneNumber;
 
+  // ignore: unused_field
   final _fromController = TextEditingController();
+  // ignore: unused_field
   final _toController = TextEditingController();
   final _senderNameController = TextEditingController();
   final _senderPhoneNumberController = TextEditingController();
@@ -28,53 +34,187 @@ class _StepOneContentState extends State<StepOneContent> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _form,
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Sender Details',
-              style: Styles.manropeExtraBold32.copyWith(
-                fontSize: 16,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Form(
+        key: _form,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Sender Details',
+                style: Styles.manropeExtraBold32.copyWith(
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Address Details',
-                    style: Styles.manropeRegular15.copyWith(
-                      fontSize: 17,
+            const SizedBox(
+              height: 20,
+            ),
+            SteponeItem(
+              widget: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Address Details',
+                      style: Styles.manropeRegular15.copyWith(
+                        fontSize: 17,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                MakeOrderSelectLocationIte(
-                  text: 'From',
-                  onPressed: () {},
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                MakeOrderSelectLocationIte(
-                  text: 'To',
-                  onPressed: () {},
-                ),
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MakeOrderSelectLocationIte(
+                    text: 'From',
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MakeOrderSelectLocationIte(
+                    text: 'To',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            SteponeItem(
+              widget: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Sender's Name",
+                      style: Styles.manropeRegular15.copyWith(
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomMainTextFormField(
+                    borderColor: Colors.transparent,
+                    fillColor: Colors.transparent,
+                    hintText: 'Input your name..',
+                    controller: _senderNameController,
+                    onChanged: (value) {
+                      senderName = value;
+                    },
+                    contentPadding: 7,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter sender's name.";
+                      }
+                      return null;
+                    },
+                    focusedBorderColor: const Color(0xff55433c),
+                    enabledBorderColor: kPrimaryColor,
+                    inputType: TextInputType.text,
+                    prefixIcon: const Icon(Icons.person),
+                    obscureText: false,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SteponeItem(
+              widget: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Sender's phone number",
+                      style: Styles.manropeRegular15.copyWith(
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomMainTextFormField(
+                    borderColor: Colors.transparent,
+                    fillColor: Colors.transparent,
+                    hintText: 'Input your number..',
+                    controller: _senderPhoneNumberController,
+                    onChanged: (value) {
+                      senderName = value;
+                    },
+                    contentPadding: 7,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter sender's phone number.";
+                      } else if (!isValidPhoneNumber(value)) {
+                        return 'Please enter a valid Egyptian phone number.';
+                      }
+                      return null;
+                    },
+                    focusedBorderColor: const Color(0xff55433c),
+                    enabledBorderColor: kPrimaryColor,
+                    inputType: TextInputType.number,
+                    prefixIcon: const Icon(Icons.phone),
+                    obscureText: false,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SteponeItem(
+              widget: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Receiver's phone number",
+                      style: Styles.manropeRegular15.copyWith(
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomMainTextFormField(
+                    borderColor: Colors.transparent,
+                    fillColor: Colors.transparent,
+                    hintText: "Input receiver's number..",
+                    controller: _receiverPhoneNumberController,
+                    onChanged: (value) {
+                      receiverPhoneNumber = value;
+                    },
+                    contentPadding: 7,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter Receiver's phone number.";
+                      } else if (!isValidPhoneNumber(value)) {
+                        return 'Please enter a valid Egyptian phone number.';
+                      }
+                      return null;
+                    },
+                    focusedBorderColor: const Color(0xff55433c),
+                    enabledBorderColor: kPrimaryColor,
+                    inputType: TextInputType.number,
+                    prefixIcon: const Icon(Icons.phone),
+                    obscureText: false,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/Features/user/post_order/presentation/views/widgets/step_one_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SteponeItem extends StatelessWidget {
+  const SteponeItem({super.key, required this.widget});
+
+  final Widget widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.4),
+            offset: const Offset(0, 4),
+            blurRadius: 8,
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: widget,
+      ),
+    );
+  }
+}
