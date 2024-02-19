a3ecec1dcb0c5988faca645e96105e8d77f58c6b
Omar_Ameer <omar.ameer244@gmail.com>
2024-02-19 20:10:48 +0200

    Scan QR Code Functionality Done.

--- lib/Features/commuter/scan qr code/first scan qr code/presentation/views/widgets/commuter_first_scan_qr_code_view_body.dart
+++ lib/Features/commuter/scan qr code/first scan qr code/presentation/views/widgets/commuter_first_scan_qr_code_view_body.dart
@@ -1,8 +1,9 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:wheel_n_deal/Core/utils/assets.dart';
+import 'package:wheel_n_deal/Core/utils/responsive.dart';
 import 'package:wheel_n_deal/Core/utils/styles.dart';
 import 'package:wheel_n_deal/Core/widgets/custom_main_button.dart';
 import 'package:wheel_n_deal/constants.dart';
@@ -17,7 +18,7 @@ class CommuterFirstScanQrCodeViewBody extends StatefulWidget {
 
 class _CommuterFirstScanQrCodeViewBodyState
     extends State<CommuterFirstScanQrCodeViewBody> {
-      String qrResult = "Scanned Data Will Appear Here";
+  String qrResult = "Scanned Data Will Appear Here";
   Future<void> scanQr() async {
     try {
       final qrCode = await FlutterBarcodeScanner.scanBarcode(
@@ -37,54 +38,68 @@ class _CommuterFirstScanQrCodeViewBodyState
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          const SizedBox(height: 26),
-          const Divider(
-            color: Color(0xFFF3F3F3),
-            thickness: 2,
-          ),
-          const SizedBox(height: 6),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Scan Package QR Code",
-              style: Styles.manropeExtraBold32.copyWith(
-                color: const Color(0xff191D31),
-                fontSize: 20,
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Divider(
+              color: Color(0xFFF3F3F3),
+              thickness: 2,
+            ),
+            const SizedBox(height: 6),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Scan Package QR Code",
+                style: Styles.manropeExtraBold32.copyWith(
+                  color: const Color(0xff191D31),
+                  fontSize: 20,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 300.43),
-          CustomMainButton(
-            onPressed: () {
-              log("Omario");
-            },
-            color: kPrimaryColor,
-            contentIsText: false,
-            widget: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  AssetsData.ordersScanQrCode,
-                  color: Colors.white,
-                ),
-                const SizedBox(
-                  width: 11.39,
-                ),
-                Text(
-                  "Scan QR Code",
-                  style: Styles.manropeMedium18.copyWith(
-                    fontWeight: FontWeight.w600,
+            SizedBox(
+              height: Responsive.screenHeight(context) * 0.36,
+            ),
+            Text(
+              qrResult,
+              style: Styles.cairoSemiBold.copyWith(
+                color: Colors.grey,
+                fontSize: 20,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            const Spacer(),
+            CustomMainButton(
+              onPressed: scanQr,
+              color: kPrimaryColor,
+              contentIsText: false,
+              widget: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AssetsData.ordersScanQrCode,
                     color: Colors.white,
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    width: 11.39,
+                  ),
+                  Text(
+                    "Scan QR Code",
+                    style: Styles.manropeMedium18.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
