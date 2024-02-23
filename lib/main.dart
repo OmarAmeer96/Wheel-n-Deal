d10b205836e7de6f93a04dde5797365b6268e731
Omar_Ameer <omar.ameer244@gmail.com>
2024-02-23 17:09:00 +0200

    Removed UnUsed Pckages 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wheel_n_deal/Core/utils/app_router.dart';
-import 'package:wheel_n_deal/firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
