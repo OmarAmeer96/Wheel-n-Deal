3be85a85fca26659cc350c731de7d8b831654a7f
Omar_Ameer <omar.ameer244@gmail.com>
2024-02-27 23:04:53 +0200

    User Track Order Screen Done.

--- lib/Features/user/tracking/presentation/views/widgets/user_track_order_view_body.dart
+++ lib/Features/user/tracking/presentation/views/widgets/user_track_order_view_body.dart
@@ -1,12 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:wheel_n_deal/Core/utils/app_router.dart';
+import 'package:wheel_n_deal/Core/utils/assets.dart';
+import 'package:wheel_n_deal/Core/utils/styles.dart';
+import 'package:wheel_n_deal/Core/widgets/custom_main_button.dart';
+import 'package:wheel_n_deal/constants.dart';
 
 class UsertrackOrderViewBody extends StatelessWidget {
   const UsertrackOrderViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Placeholder(),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Picking up your order",
+                style: Styles.manropeExtraBold18.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Expanded(
+              child: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(31, 41),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            CustomMainButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return SizedBox(
+                      width: double.infinity,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: 60,
+                              height: 6,
+                              decoration: ShapeDecoration(
+                                color: const Color(0xFFA3A3A3),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                SizedBox(
+                                  height: 64,
+                                  child: Image.asset(
+                                    AssetsData.profileImage,
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: -10,
+                                  left: -1,
+                                  right: -1,
+                                  child: Card(
+                                    elevation:
+                                        8.0, // Adjust the elevation value as needed
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(999),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 6,
+                                        vertical: 2,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          const Text(
+                                            "4.5",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                          const Spacer(),
+                                          SvgPicture.asset(
+                                            AssetsData.starIcon,
+                                            height: 20,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Ahmed is taking care of your order today",
+                              style: Styles.manropeExtraBold18.copyWith(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                GoRouter.of(context).push(AppRouter
+                                    .kCommuterProfileSeenByUserFromTrackingView);
+                              },
+                              borderRadius: BorderRadius.circular(8),
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: Text(
+                                  "View profile",
+                                  style: Styles.manropeExtraBold18.copyWith(
+                                    fontSize: 16,
+                                    color: kPrimaryColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              text: "Chat",
+              color: kPrimaryColor,
+              height: 45,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
