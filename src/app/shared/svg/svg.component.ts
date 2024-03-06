19c637ec4d08ab23ce2c33b7a48173d319dcc01b
Omar-Zantout <omar82199@gmail.com>
2024-03-06 20:51:41 +0200

    small css modifcation

--- src/app/shared/svg/svg.component.ts
+++ src/app/shared/svg/svg.component.ts
@@ -6,42 +6,57 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, map } from 'rxjs';
+import { SvgIconService } from '../../core/services/svg-icon.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-svg',
   standalone: true,
-  imports: [],
-  template: `<span [class]="class" [innerHTML]="svgIcon"></span>`,
+  imports: [CommonModule],
+  template: `<span [class]="class" [innerHTML]="svgIcon | async"></span>`,
   // templateUrl: './svg.component.html',
   styleUrl: './svg.component.scss',
 })
 export class SvgComponent implements OnInit, OnDestroy {
   @Input() public name!: string;
   @Input() public class!: string;
-  public svgIcon!: SafeHtml;
+  public svgIcon!: Observable<SafeHtml>;
+  private svgSubscription!: Subscription;
 
-  constructor(private _http: HttpClient, private _sanitizer: DomSanitizer) {}
+  constructor(private _sanitizer: DomSanitizer, private _svg: SvgIconService) {}
 
   ngOnInit(): void {
-    const assets: Observable<string> = this._http.get(
-      `assets/SVGs/${this.name}.svg`,
-      {
-        responseType: 'text',
-      }
-    );
-
-    assets.subscribe({
-      next: (value) => {
-        this.svgIcon = this._sanitizer.bypassSecurityTrustHtml(value);
-      },
-      error(err) {
-        console.error(err);
-      },
-    });
+    this.svgIcon = this._svg
+      .getSvg(this.name)
+      .pipe(map((value) => this._sanitizer.bypassSecurityTrustHtml(value)));
+    // Initialize the subscription in ngOnInit
+    this.svgSubscription = this.svgIcon.subscribe();
   }
-  ngOnDestroy(): void {}
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the subscription
+    this.svgSubscription?.unsubscribe();
+  }
+
+  // ngOnInit(): void {
+  //   const assets: Observable<string> = this._http.get(
+  //     `assets/SVGs/${this.name}.svg`,
+  //     {
+  //       responseType: 'text',
+  //     }
+  //   );
+
+  //   this._svg.getSvg(this.name).subscribe({
+  //     next: (value) => {
+  //       this.svgIcon = this._sanitizer.bypassSecurityTrustHtml(value);
+  //     },
+  //     error(err) {
+  //       console.error(err);
+  //     },
+  //   });
+  // }
+
   // ngOnChanges(changes: SimpleChanges): void {
   //   this._http
   //     .get(`assets/SVGs/${this.name}.svg`, { responseType: 'text' })
