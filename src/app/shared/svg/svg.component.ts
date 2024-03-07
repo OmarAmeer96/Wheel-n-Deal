957d14fba9596ab4461bd2cf25b647953071855c
Omar-Zantout <omar82199@gmail.com>
2024-03-07 20:28:14 +0200

    clean svg code

--- src/app/shared/svg/svg.component.ts
+++ src/app/shared/svg/svg.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Observable, Subscription, map } from 'rxjs';
 import { SvgIconService } from '../../core/services/svg-icon.service';
@@ -14,7 +7,6 @@ import { CommonModule } from '@angular/common';
   selector: 'app-svg',
   standalone: true,
   imports: [CommonModule],
-  // template: `<span [class]="class" [innerHTML]="svgIcon | async"></span>`,
   templateUrl: './svg.component.html',
   styleUrl: './svg.component.scss',
 })
@@ -30,43 +22,10 @@ export class SvgComponent implements OnInit, OnDestroy {
     this.svgIcon = this._svg
       .getSvg(this.name)
       .pipe(map((value) => this._sanitizer.bypassSecurityTrustHtml(value)));
-    // Initialize the subscription in ngOnInit
     this.svgSubscription = this.svgIcon.subscribe();
   }
 
   ngOnDestroy(): void {
-    // Unsubscribe from the subscription
     this.svgSubscription?.unsubscribe();
   }
-
-  // ngOnInit(): void {
-  //   const assets: Observable<string> = this._http.get(
-  //     `assets/SVGs/${this.name}.svg`,
-  //     {
-  //       responseType: 'text',
-  //     }
-  //   );
-
-  //   this._svg.getSvg(this.name).subscribe({
-  //     next: (value) => {
-  //       this.svgIcon = this._sanitizer.bypassSecurityTrustHtml(value);
-  //     },
-  //     error(err) {
-  //       console.error(err);
-  //     },
-  //   });
-  // }
-
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   this._http
-  //     .get(`assets/SVGs/${this.name}.svg`, { responseType: 'text' })
-  //     .subscribe({
-  //       next: (value) => {
-  //         this.svgIcon = this._sanitizer.bypassSecurityTrustHtml(value);
-  //       },
-  //       error(err) {
-  //         console.error(err);
-  //       },
-  //     });
-  // }
 }
