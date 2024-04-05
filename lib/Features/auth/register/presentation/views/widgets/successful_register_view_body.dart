f8d54bd0cd0b5c4ff9ed2ac1cd86b8124d5eff63
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-05 19:42:58 +0200

    Add the errors and use_cases classes fies.

--- lib/Features/auth/register/presentation/views/widgets/successful_register_view_body.dart
+++ lib/Features/auth/register/presentation/views/widgets/successful_register_view_body.dart
@@ -64,6 +64,7 @@ class SuccessfulRegisterViewBody extends StatelessWidget {
                 const SizedBox(
                   width: 40,
                 ),
+                
                 CustomMainButton(
                   width: Responsive.screenWidth(context) * 0.37,
                   text: 'Commuter',
