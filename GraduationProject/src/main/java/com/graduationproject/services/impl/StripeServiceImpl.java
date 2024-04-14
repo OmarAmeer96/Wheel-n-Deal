1de6e0ab066e36bc201a56f98acee3265865fb3f
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2024-04-14 13:21:29 +0200

    update

--- GraduationProject/src/main/java/com/graduationproject/services/impl/StripeServiceImpl.java
+++ GraduationProject/src/main/java/com/graduationproject/services/impl/StripeServiceImpl.java
@@ -1,10 +1,6 @@
 package com.graduationproject.services.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.graduationproject.DTOs.stripePaymentDTOs.ChargeUserDTO;
-import com.graduationproject.DTOs.stripePaymentDTOs.CreateStripeUserRequestDTO;
 import com.graduationproject.entities.StripePaymentEntity;
 import com.graduationproject.entities.User;
 import com.graduationproject.repositories.StripePaymentRepository;
@@ -12,10 +8,7 @@ import com.graduationproject.repositories.UserRepository;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.*;
-import com.stripe.param.CustomerCreateParams;
-import com.stripe.param.CustomerFundCashBalanceParams;
 import com.stripe.param.PaymentIntentCreateParams;
-import com.stripe.param.PaymentSourceCollectionCreateParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,44 +27,6 @@ public class StripeServiceImpl {
     private StripePaymentRepository paymentRepository;
     @Autowired
     private UserRepository userRepository;
-
-    /**
-     * Creates a new Stripe user based on the provided request.
-     * @param request The request containing user details
-     * @return The ID of the created Stripe user
-     * @throws StripeException If an error occurs during the Stripe API call
-     * @throws JsonProcessingException If an error occurs during JSON processing
-     */
-    public String createStripeUser(CreateStripeUserRequestDTO request) throws StripeException, JsonProcessingException {
-        // Set the API key for Stripe
-        Stripe.apiKey = "sk_test_51Of0HSDRpAtfI02p07kURFyWFuON9GhxXSEzZNxRpbVqLXc83KH0JcMjeURgkwf6UXsD9Xm7Z7sVf3g9tFC2Gdeo00fPYbS9G6";
-
-        // Create customer parameters
-        CustomerCreateParams customerParams = CustomerCreateParams.builder()
-                .setPhone(request.getPhoneNumber())
-                .setName(request.getUserName())
-                .build();
-
-        // Create a new customer in Stripe
-        Customer customer = Customer.create(customerParams);
-
-        // Retrieve customer details
-        Customer customerRetrieve = Customer.retrieve(customer.getId());
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(customerRetrieve.toJson());
-        Map<String, String> customerDetails = new HashMap<>();
-        jsonNode.fields().forEachRemaining(entry -> customerDetails.put(entry.getKey(), entry.getValue().asText()));
-
-        // Save user details in the database
-        User userEntity = new User();
-        userEntity.setStripeId(customer.getId());
-        userEntity.setPhoneNumber(request.getPhoneNumber());
-        userEntity.setUsername(request.getUserName());
-        userEntity.setAmount(0L);
-        userRepository.save(userEntity);
-
-        return customer.getId();
-    }
 
     /**
      * Charges a user's account based on the provided details.
