081ff6dc5072857eee2e67e3f1b763ac8e071228
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2024-04-18 05:30:59 +0200

    update

--- GraduationProject/src/main/java/com/graduationproject/entities/PaymobResponse.java
+++ GraduationProject/src/main/java/com/graduationproject/entities/PaymobResponse.java
@@ -1,6 +1,9 @@
 package com.graduationproject.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -12,6 +15,12 @@ public class PaymobResponse {
     private Long id;
     private Long externalId;
     private boolean pending;
+
+    @NotNull(message = "Amount must not be null")
+    @Min(value = 1, message = "Amount must be greater than 0")
     private Integer amountCents;
+
+    @NotNull
+    @NotBlank
     private String currency;
 }
