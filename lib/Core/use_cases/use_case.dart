0802fbaaeed8fb9bc550d70a0aa9deb026388ebe
Omar_Ameer <omar.ameer244@gmail.com>
2024-04-24 18:30:17 +0200

    Refactor core files, remove unused code, and clean up imports

--- lib/Core/use_cases/use_case.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:wheel_n_deal/Core/errors/failure.dart';
-
-abstract class UseCase<Type, Param> {
-  Future<Either<Failure, Type>> call([Param param]);
-}
-
-class NoParam {}
