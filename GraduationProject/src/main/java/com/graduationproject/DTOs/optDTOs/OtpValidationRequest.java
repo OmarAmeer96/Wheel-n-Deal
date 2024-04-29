65f278208d9ee802231887686c9551352bd357a9
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2024-04-29 16:38:31 +0200

    modify otp

--- GraduationProject/src/main/java/com/graduationproject/DTOs/optDTOs/OtpValidationRequest.java
+++ GraduationProject/src/main/java/com/graduationproject/DTOs/optDTOs/OtpValidationRequest.java
@@ -4,16 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Data Transfer Object (DTO) representing the request for OTP validation.
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OtpValidationRequest {
-    private String newPassword;
-
     private String phoneNumber;
-
     private String otpNumber;
 }
