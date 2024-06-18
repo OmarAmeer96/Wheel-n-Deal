6507a5ccb6b4b18d35669a74559f7b01615c29b1
Omar-Zantout <omar82199@gmail.com>
2024-06-18 18:34:05 +0300

    card component

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { HCardsComponent } from '../../shared/widgets/h-cards/h-cards.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss',
-  imports: [HCardsComponent],
+  imports: [HCardsComponent, CommonModule],
 })
-export class OrdersComponent {}
+export class OrdersComponent {
+  cards = [
+    { record: '849', label: 'Total orders' },
+    { record: '123', label: 'Pending orders' },
+    // ...
+  ];
+}
--- src/app/shared/widgets/h-cards/h-cards.component.ts
+++ src/app/shared/widgets/h-cards/h-cards.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SvgComponent } from '../svg/svg.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-h-cards',
   standalone: true,
-  imports: [],
   templateUrl: './h-cards.component.html',
-  styleUrl: './h-cards.component.scss'
+  styleUrl: './h-cards.component.scss',
+  imports: [SvgComponent, CommonModule],
 })
 export class HCardsComponent {
-
+  @Input() icon!: string;
+  @Input() record!: string;
+  @Input() label!: string;
 }
