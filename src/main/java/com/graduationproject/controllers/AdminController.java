f14f5253b2f682dd00a6beab70fc1d0a2c1582c8
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2024-06-19 02:11:33 +0300

    add CrossOrigin

--- src/main/java/com/graduationproject/controllers/AdminController.java
+++ src/main/java/com/graduationproject/controllers/AdminController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/admin")
 @RequiredArgsConstructor
--- src/main/java/com/graduationproject/controllers/AuthenticationController.java
+++ src/main/java/com/graduationproject/controllers/AuthenticationController.java
@@ -5,11 +5,9 @@ import com.graduationproject.services.AuthenticationService;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
